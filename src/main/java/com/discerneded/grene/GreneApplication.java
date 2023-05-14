9cdb2278eb5ec3b9286ce0442595c89ebb6a6392
Jonas Moloto <jonasmoloto@outlook.com>
2023-05-14 19:32:49 +0200

    remove jquery

--- src/main/java/com/discerneded/grene/GreneApplication.java
+++ src/main/java/com/discerneded/grene/GreneApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,25 +20,21 @@ public class GreneApplication {
 	}
 
 	@GetMapping("/")
-	public String home() {
-		return "index.html";
-	}
-
-	@GetMapping("/password")
-	@ResponseBody
-	public String generatePassword() {
+	public String home(Model model) {
 		String upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		String lowerCaseLetters = upperCaseLetters.toLowerCase();
 		String numbers = "1234567890";
 		String specialCharacters = "!@#$%^&*()_-+=";
 		String combinedString = upperCaseLetters+lowerCaseLetters+numbers+specialCharacters;
-		int passwordLength = 10;
+		int passwordLength = 15;
 		char[] password = new char[passwordLength];
 		Random random = new Random();
 
 		for (int i = 0; i < passwordLength; i++) {
 			password[i] = combinedString.charAt(random.nextInt(combinedString.length()));
 		}
-		return new String(password);
+		model.addAttribute("password", new String(password));
+		return "index.html";
 	}
+
 }
