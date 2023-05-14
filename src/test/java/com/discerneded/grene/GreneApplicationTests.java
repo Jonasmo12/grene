cdeb542bd92ddef092b34acae3a306f1b2d3789e
Jonas Moloto <jonasmoloto@outlook.com>
2023-05-14 15:33:11 +0200

    project init

---
+++ src/test/java/com/discerneded/grene/GreneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discerneded.grene;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
