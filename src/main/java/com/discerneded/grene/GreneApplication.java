b2aad1862e09a87d2bfd245214960a38b599f390
Jonas Moloto <jonasmoloto@outlook.com>
2023-05-14 17:32:14 +0200

    password generation sequence

--- src/main/java/com/discerneded/grene/GreneApplication.java
+++ src/main/java/com/discerneded/grene/GreneApplication.java
@@ -2,8 +2,13 @@ package com.discerneded.grene;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Random;
 
 @Controller
 @SpringBootApplication
@@ -16,5 +21,23 @@ public class GreneApplication {
 	@GetMapping("/")
 	public String home() {
 		return "index.html";
+	}
+
+	@GetMapping("/password")
+	@ResponseBody
+	public String generatePassword() {
+		String upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String lowerCaseLetters = upperCaseLetters.toLowerCase();
+		String numbers = "1234567890";
+		String specialCharacters = "!@#$%^&*()_-+=";
+		String combinedString = upperCaseLetters+lowerCaseLetters+numbers+specialCharacters;
+		int passwordLength = 10;
+		char[] password = new char[passwordLength];
+		Random random = new Random();
+
+		for (int i = 0; i < passwordLength; i++) {
+			password[i] = combinedString.charAt(random.nextInt(combinedString.length()));
+		}
+		return new String(password);
 	}
 }
